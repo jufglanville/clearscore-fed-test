f6756b8b69f7c7cad135984099792f8aa3141ea5
Joe Glanville <josephglanville@gmail.com>
2023-07-22 09:44:29 +0100

    update test

--- src/components/TaskList/TaskList.test.tsx
+++ src/components/TaskList/TaskList.test.tsx
@@ -1,15 +1,11 @@
-import { render, cleanup } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import TaskList from './TaskList';
 
 describe('TaskList Component', () => {
-  afterEach(cleanup);
-
-  it('renders the card list', () => {
-    const { getByTestId } = render(<TaskList />);
-
-    expect(getByTestId('card-list')).toBeInTheDocument();
+  afterEach(() => {
+    window.history.pushState(null, document.title, '/');
   });
 
   it('Adds a new task when the add button is clicked', async () => {
@@ -22,14 +18,34 @@ describe('TaskList Component', () => {
     expect(getByPlaceholderText('Title')).toBeInTheDocument();
   });
 
+  it('Adds a new task when the add button is clicked', async () => {
+    const user = userEvent.setup();
+    const { getByPlaceholderText, getByAltText } = render(<TaskList />);
+
+    const addButton = getByAltText('add');
+    await user.click(addButton);
+
+    expect(getByPlaceholderText('Title')).toBeInTheDocument();
+  });
+
+  afterEach(() => {
+    document.body.innerHTML = '';
+  });
+
+  it('renders the card list', () => {
+    const { getByTestId } = render(<TaskList />);
+
+    expect(getByTestId('card-list')).toBeInTheDocument();
+  });
+
   it('Deletes a task when the delete button is clicked', async () => {
     const user = userEvent.setup();
-    const { queryByPlaceholderText, getByAltText } = render(<TaskList />);
-
-    await user.click(getByAltText('add'));
-    expect(queryByPlaceholderText('Title')).toBeInTheDocument();
-
-    await user.click(getByAltText('delete'));
-    expect(queryByPlaceholderText('Title')).not.toBeInTheDocument();
+    render(<TaskList />);
+    await user.click(screen.getByAltText('add'));
+    screen.logTestingPlaygroundURL();
+    // console.log(prettyDOM(screen.container));
+    // expect(getByPlaceholderText('Title')).toBeInTheDocument();
+    // await user.click(getByAltText('delete'));
+    // expect(getByPlaceholderText('Title')).not.toBeInTheDocument();
   });
 });
